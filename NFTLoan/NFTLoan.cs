d1f9a74cd34aa0c071e3112d79514e7764c020de
Hecate2 <2474101468@qq.com>
2022-04-10 20:51:41 +0800

    mint new NFT tokens representing right of use

--- NFTLoan/NFTLoan.cs
+++ NFTLoan/NFTLoan.cs
@@ -12,21 +12,29 @@ namespace NFTLoan
     [ManifestExtra("Author", "Hecate2")]
     [ManifestExtra("Email", "developer@neo.org")]
     [ManifestExtra("Description", "NFTFlashLoan")]
-    public class NFTLoan : SmartContract
+    public class NFTLoan : DivisibleNep11Token<DivisibleNep11TokenState>
     {
         private const byte PREFIX_TOKEN_FOR_RENTAL = (byte)'r';  // token + (ByteString)(BigInteger)tokenId.Length + tokenId + renter -> StdLib.Serialize(amount, price)
         private const byte PREFIX_TOKEN_OF_OWNER = (byte)'o';    // renter + token + tokenId -> StdLib.Serialize(amount, price)
+        private const byte PREFIX_TOKENID_INTERNAL_TO_EXTERNAL = (byte)'i';  // internal tokenId -> external token contract + tokenId
+        private const byte PREFIX_TOKENID_EXTERNAL_TO_INTERNAL = (byte)'e';  // external token contract + tokenId -> internal tokenId
+        private const byte PREFIX_RENTAL_DEADLINE = (byte)'d';    // ?? renter + tenant + amount + internalTokenId + start time -> deadline
 
         public static void OnNEP11Payment(UInt160 from, BigInteger amount, ByteString tokenId, BigInteger data) { }
+        public override string Symbol() => "NEPHRENT";
         public static BigInteger GetDecimals(UInt160 token) => (BigInteger)Contract.Call(token, "decimals", CallFlags.ReadStates);
         public static Iterator GetTokenForRental(UInt160 token) => new StorageMap(Storage.CurrentContext, PREFIX_TOKEN_FOR_RENTAL).Find(token, FindOptions.RemovePrefix);
         public static Iterator GetMyTokenForRental(UInt160 renter) => new StorageMap(Storage.CurrentContext, PREFIX_TOKEN_OF_OWNER).Find(renter, FindOptions.RemovePrefix);
+        public static ByteString GetExternalTokenInfo(ByteString internalTokenId) => new StorageMap(Storage.CurrentContext, PREFIX_TOKENID_INTERNAL_TO_EXTERNAL).Get(internalTokenId);
+        public static Iterator ListExternalTokenInfo(ByteString prefix) => new StorageMap(Storage.CurrentContext, PREFIX_TOKENID_EXTERNAL_TO_INTERNAL).Find(prefix);
+        public static ByteString GetInternalTokenId(UInt160 externalTokenContract, ByteString externalTokenId) => new StorageMap(Storage.CurrentContext, PREFIX_TOKENID_EXTERNAL_TO_INTERNAL).Get(externalTokenContract + externalTokenId);
+        public static Iterator ListInternalTokenId(ByteString prefix) => new StorageMap(Storage.CurrentContext, PREFIX_TOKENID_INTERNAL_TO_EXTERNAL).Find(prefix);
 
         public static void SetRentalPrice(UInt160 renter, UInt160 token, ByteString tokenId, BigInteger price)
         {
             ExecutionEngine.Assert(Runtime.CheckWitness(renter), "No witness");
 
-            StorageMap tokenForRental = new StorageMap(Storage.CurrentContext, PREFIX_TOKEN_FOR_RENTAL);
+            StorageMap tokenForRental = new(Storage.CurrentContext, PREFIX_TOKEN_FOR_RENTAL);
             ByteString key = token + (ByteString)(BigInteger)tokenId.Length + tokenId + renter;
             BigInteger[] amountAndPrice = (BigInteger[])StdLib.Deserialize(tokenForRental.Get(key));
             ExecutionEngine.Assert(amountAndPrice[0] > 0, "No token at rental");
@@ -34,37 +42,67 @@ namespace NFTLoan
             ByteString serialized = StdLib.Serialize(amountAndPrice);
             tokenForRental.Put(key, serialized);
 
-            StorageMap tokenOfOwner = new StorageMap(Storage.CurrentContext, PREFIX_TOKEN_OF_OWNER);
+            StorageMap tokenOfOwner = new (Storage.CurrentContext, PREFIX_TOKEN_OF_OWNER);
             tokenOfOwner.Put(renter + token + tokenId, serialized);
         }
 
-        public static BigInteger RegisterRental(UInt160 renter, UInt160 token, BigInteger amountForRent, ByteString tokenId, BigInteger price)
+        private static ByteString MintSubTokenForRental(UInt160 owner, UInt160 originalContract, ByteString externalTokenId, BigInteger amount)
+        {
+            StorageContext context = Storage.CurrentContext;
+            StorageMap externalToInternal = new(context, PREFIX_TOKENID_EXTERNAL_TO_INTERNAL);
+            ByteString externalTokenInfo = originalContract + externalTokenId;
+            UInt160 internalTokenId = (UInt160)externalToInternal.Get(externalTokenInfo);
+            if (internalTokenId == UInt160.Zero)
+            {
+                internalTokenId = (UInt160)NewTokenId();
+                externalToInternal.Put(externalTokenId, internalTokenId);
+                new StorageMap(context, PREFIX_TOKENID_INTERNAL_TO_EXTERNAL).Put(internalTokenId, externalTokenInfo);
+            }
+            Mint(owner, amount, internalTokenId, new DivisibleNep11TokenState { Name = externalTokenInfo });
+            // Ideally mint the new tokens for this contract, and immediately register the new tokens for ordinary rental
+            return internalTokenId;
+        }
+
+        private static void RegisterOrdinaryLoan(UInt160 renter, BigInteger amount, ByteString internalTokenId, BigInteger price)
+        {
+            // TODO
+        }
+
+        public static BigInteger RegisterRental(
+            UInt160 renter, UInt160 tokenContract, BigInteger amountForRent, ByteString tokenId,
+            BigInteger flashLoanPrice, BigInteger ordinaryLoanPrice)
         {
             ExecutionEngine.Assert(tokenId.Length <= 64, "tokenId.Length > 64");
-            BigInteger decimals = GetDecimals(token);
+            ExecutionEngine.Assert(tokenContract != Runtime.ExecutingScriptHash, "Cannot register rental for tokens issued by this contract");
+            BigInteger decimals = GetDecimals(tokenContract);
             // ExecutionEngine.Assert(amountForRent > 0, "amountForRent <= 0");  // unnecessary
             // Transfer is very risky. Consider a whitelist of tokens. 
             if (decimals == 0)
             {
-                ExecutionEngine.Assert((bool)Contract.Call(token, "transfer", CallFlags.All, new object[] { Runtime.ExecutingScriptHash, tokenId, null }), "Transfer failed");
+                ExecutionEngine.Assert((bool)Contract.Call(tokenContract, "transfer", CallFlags.All, new object[] { Runtime.ExecutingScriptHash, tokenId, null }), "Transfer failed");
                 amountForRent = 1;
             }
             else
             {
-                ExecutionEngine.Assert((bool)Contract.Call(token, "transfer", CallFlags.All, new object[] { renter, Runtime.ExecutingScriptHash, amountForRent, tokenId, null }), "Transfer failed");
+                ExecutionEngine.Assert((bool)Contract.Call(tokenContract, "transfer", CallFlags.All, new object[] { renter, Runtime.ExecutingScriptHash, amountForRent, tokenId, null }), "Transfer failed");
             }
 
             StorageContext context = Storage.CurrentContext;
             
             StorageMap tokenForRental = new(context, PREFIX_TOKEN_FOR_RENTAL);
-            ByteString key = token + (ByteString)(BigInteger)tokenId.Length + tokenId + renter;
+            ByteString key = tokenContract + (ByteString)(BigInteger)tokenId.Length + tokenId + renter;
             BigInteger[] amountAndPrice = (BigInteger[])StdLib.Deserialize(tokenForRental.Get(key));
             amountAndPrice[0] += amountForRent;
+            amountAndPrice[1] = flashLoanPrice;
             ByteString serialized = StdLib.Serialize(amountAndPrice);
-            tokenForRental.Put(token + (ByteString)(BigInteger)tokenId.Length + tokenId + renter, serialized);
+            tokenForRental.Put(tokenContract + (ByteString)(BigInteger)tokenId.Length + tokenId + renter, serialized);
 
             StorageMap tokenOfOwner = new(context, PREFIX_TOKEN_OF_OWNER);
-            tokenOfOwner.Put(renter + token + tokenId, serialized);
+            tokenOfOwner.Put(renter + tokenContract + tokenId, serialized);
+
+            ByteString internalTokenId = MintSubTokenForRental(renter, tokenContract, tokenId, amountForRent);
+            RegisterOrdinaryLoan(renter, amountForRent, internalTokenId, ordinaryLoanPrice);
+
             return amountAndPrice[0];
         }
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #### ROADMAP
 
-1. Flash loan for divisible and non-divisible NFTs
-2. [Aborted: no need to mint anything] Contract to mint new NFTs from existing NFTs. The new minted NFTs represents the permission to **use** the original NFT. 
+1. [Done] Flash loan for divisible and non-divisible NFTs
+2. [Done] Contract to mint new NFTs from existing NFTs. The new minted NFTs represents the permission to **use** the original NFT. 
 3. Ordinary rental of the right of NFT usage. 
 
